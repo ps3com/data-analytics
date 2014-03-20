0071707fd7f5306791d8ab1c67d5458f505b81ee
ps3com40 <ps3com40@gmail.com>
2014-03-20 14:02:15 +0000

    test

--- startup.R
+++ startup.R
@@ -8,7 +8,7 @@ frame_files <- lapply(sys.frames(), function(x) x$ofile)
 frame_files <- Filter(Negate(is.null), frame_files)
 PATH <- dirname(frame_files[[length(frame_files)]])
 setwd(paste(PATH, "/uk/ac/bolton/trailer-analytics/view/", sep=""))
-# get shiny.
+# get shiny..
 library(shiny)
 # start the shiny app
 runApp()
